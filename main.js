1db00cc7a7cf4f79ad1abaa517538caadf8eff86
Vlad-Petruk <vladpetruk.00@gmail.com>
2023-09-26 13:01:45 +0300

    with function that displays cards

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+const container = document.querySelector('.main-contaner')
+const books = document.querySelector('.books');
+
+
+
+
+
 const myLibrary = [
 {
     title: 'A Game of Thrones',
@@ -29,4 +36,24 @@ function addBookToLibrary(){
 
 myLibrary.forEach((book)=>{
 
+    let bookCard = document.createElement('div')
+    bookCard.setAttribute('class','book')
+    books.append(bookCard);
+
+    let title = document.createElement('p');
+    title.setAttribute('class','info-element')
+    let author = document.createElement('p');
+    let pages = document.createElement('p');
+    let read = document.createElement('p');
+    
+    bookCard.append(title);
+    bookCard.append(author);
+    bookCard.append(pages);
+    bookCard.append(read);
+
+    title.innerHTML = `Book title: ${book.title}`;
+    author.innerHTML = `Author: ${book.author}`;
+    pages.innerHTML = `Number of pages: ${book.pages}`;
+    read.innerHTML = `Read status: ${book.read}`;
+    console.log(title)
 })
