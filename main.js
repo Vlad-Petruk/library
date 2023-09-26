cd9cfb93196807f170d6795f92e08bce6cff87fb
Vlad-Petruk <vladpetruk.00@gmail.com>
2023-09-26 17:57:13 +0300

    added displayLibrary, removeBook, toggleReadStatus functions

--- main.js
+++ main.js
@@ -1,23 +1,35 @@
-const container = document.querySelector('.main-contaner')
+const container = document.querySelector('.main-container')
 const books = document.querySelector('.books');
 
+const newBookBtn = document.querySelector('.add-book')
+const bookDial = document.getElementById('bookDial');
+const confirmBtn = bookDial.querySelector("#confirmBtn");
+const inputTitle = bookDial.querySelector('#book-title');
+const inputAuthor = bookDial.querySelector('#book-author')
+const inputPages = bookDial.querySelector('#book-pages')
+const inputRead = bookDial.querySelector('#book-read')
 
+
+let userTitle = '';
+let userAuthor = '';
+let userPages = '';
+let userRead = '';
 
 
 
 const myLibrary = [
-{
-    title: 'A Game of Thrones',
-    author: 'George R. R. Martin',
-    pages: '694',
-    read: 'Yes'
-},
-{
-    title: 'The Blade Itself',
-    author: 'Joe Abercrombie',
-    pages: '529',
-    read: 'Yes'
-}
+// {
+//     title: 'A Game of Thrones',
+//     author: 'George R. R. Martin',
+//     pages: '694',
+//     read: 'Yes'
+// },
+// {
+//     title: 'The Blade Itself',
+//     author: 'Joe Abercrombie',
+//     pages: '529',
+//     read: 'Yes'
+// }
 ];
 
 function Book(title,author,pages,read){
@@ -30,11 +42,57 @@ function Book(title,author,pages,read){
     }
 }
 
-function addBookToLibrary(){
+newBookBtn.addEventListener("click", () => {
+    inputTitle.value = '';
+    bookDial.showModal();
+  });
+  
+inputTitle.addEventListener("input", (e) => {
+    userTitle = inputTitle.value;
+  });
 
+inputAuthor.addEventListener("input", (e) => {
+userAuthor = inputAuthor.value;
+});
+
+inputPages.addEventListener("input", (e) => {
+userPages = inputPages.value;
+});
+
+inputRead.addEventListener("input", (e) => {
+userRead = inputRead.value;
+});
+
+confirmBtn.addEventListener("click", (event) => {
+    event.preventDefault(); 
+    const book = new Book (userTitle,userAuthor,userPages,userRead)
+    myLibrary.push(book)
+    displayLibrary()
+    // addBookToLibrary()
+    bookDial.close(); 
+});
+
+// function addBookToLibrary(){
+// const book = new Book (userTitle,userAuthor,userPages,userRead)
+// myLibrary.push(book)
+// }
+
+function removeBook(index) {
+    myLibrary.splice(index, 1);
 }
 
-myLibrary.forEach((book)=>{
+function toggleReadStatus(index) {
+    myLibrary[index].hasRead = !myLibrary[index].hasRead;
+    displayLibrary();
+  }
+
+function displayLibrary (){
+
+while (books.firstChild) {
+    books.removeChild(books.firstChild);
+    }
+
+myLibrary.forEach((book,index)=>{
 
     let bookCard = document.createElement('div')
     bookCard.setAttribute('class','book')
@@ -45,15 +103,29 @@ myLibrary.forEach((book)=>{
     let author = document.createElement('p');
     let pages = document.createElement('p');
     let read = document.createElement('p');
+    let removeBtn = document.createElement('button');
+    let togleReadBtn = document.createElement('button')
+    removeBtn.textContent = 'Remove';
+    togleReadBtn.textContent = 'Toggle Read status'
     
     bookCard.append(title);
     bookCard.append(author);
     bookCard.append(pages);
     bookCard.append(read);
+    bookCard.append(removeBtn);
+    bookCard.append(togleReadBtn)
 
     title.innerHTML = `Book title: ${book.title}`;
     author.innerHTML = `Author: ${book.author}`;
     pages.innerHTML = `Number of pages: ${book.pages}`;
-    read.innerHTML = `Read status: ${book.read}`;
-    console.log(title)
+    read.innerHTML = `Read status: ${book.hasRead ? 'Yes' : 'No'}`;
+    removeBtn.addEventListener('click',()=>{
+        removeBook(`${index}`)
+        displayLibrary();
+    })
+    togleReadBtn.addEventListener('click',()=>{
+        toggleReadStatus(`${index}`)
+    })
 })
+}
+
